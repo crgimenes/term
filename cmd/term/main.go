1074d1e1b53cb7e428e9db4718f700b25494e1a4
Cesar Gimenes <crg@crg.eti.br>
2021-01-01 16:19:57 -0300

    removes unused variables

--- cmd/term/main.go
+++ cmd/term/main.go
@@ -19,10 +19,6 @@ var ct = Console.New()
 type out struct {
 }
 
-var ansiMap = make(map[string]int)
-var ansiAux string
-var ansiRec bool
-
 func isLetter(c byte) bool {
 	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
 }
